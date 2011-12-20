2b5d01635ed19f43407c65523ff9f84698e3b657
luyanfei78@gmail.com <luyanfei78@gmail.com@614c841e-ecf4-75f3-70a6-111a6c103a0b>
2011-12-20 07:04:44 +0000

    Tetris项目布局完成。

---
+++ java/swt/Tetris/trunk/src/cn/jhc/swt/tetris/model/Shape.java
@@ -0,0 +1,24 @@
+package cn.jhc.swt.tetris.model;
+
+public class Shape {
+	
+	public void moveDown() {
+		
+	}
+	
+	public void moveLeft() {
+		
+	}
+	
+	public void moveRight() {
+		
+	}
+	
+	public void rotate() {
+		
+	}
+	
+	public void drawMe() {
+		
+	}
+}
---
+++ java/swt/Tetris/trunk/src/cn/jhc/swt/tetris/model/ShapeFactory.java
@@ -0,0 +1,7 @@
+package cn.jhc.swt.tetris.model;
+
+public class ShapeFactory {
+	public static Shape getShape() {
+		return new Shape();
+	}
+}
