bc02f1647d8c93153726da9e42c7e57f860032e4
luyanfei78@gmail.com <luyanfei78@gmail.com@614c841e-ecf4-75f3-70a6-111a6c103a0b>
2012-04-20 02:07:47 +0000

    focus-crawler:完成了ContextDAO的第二个测试。

--- java/focus-crawler/trunk/src/test/java/cn/jhc/heritrix/db/dao/ContextDAOTest.java
+++ java/focus-crawler/trunk/src/test/java/cn/jhc/heritrix/db/dao/ContextDAOTest.java
@@ -1,11 +1,28 @@
 package cn.jhc.heritrix.db.dao;
 
+import org.dbunit.Assertion;
+import org.dbunit.dataset.ITable;
+import org.dbunit.util.fileloader.DataFileLoader;
+import org.dbunit.util.fileloader.FlatXmlDataFileLoader;
 import org.junit.Test;
 
 import cn.jhc.heritrix.db.FocusDBTestCase;
+import cn.jhc.heritrix.db.bean.Context;
 
 public class ContextDAOTest extends FocusDBTestCase {
 
+	@Test
+	public void testInsert() throws Exception{
+		Context ctx = new Context("5",5,10);
+		ContextDAO dao = DAOFactory.getContextDAO();
+		dao.insert(ctx);
+		ITable actualTable = getConnection().createQueryTable("context", 
+				"select path,instance_id,context_level from context");
+		DataFileLoader loader = new FlatXmlDataFileLoader();
+		ITable expectedTable = loader.load("/context_dataset.xml").getTable("context_select");
+		Assertion.assertEquals(expectedTable, actualTable);
+	}
+	
 	@Test
 	public void testGetContextID() throws Exception {
 		ContextDAO dao = DAOFactory.getContextDAO();
