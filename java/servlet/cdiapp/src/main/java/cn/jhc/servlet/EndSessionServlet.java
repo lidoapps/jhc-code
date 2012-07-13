29943a2335c66b30eedebfa4780b360b32146e02
luyanfei78@gmail.com <luyanfei78@gmail.com@614c841e-ecf4-75f3-70a6-111a6c103a0b>
2012-07-13 07:53:32 +0000

    cdiapp:添加了结束会话功能。

---
+++ java/servlet/cdiapp/src/main/java/cn/jhc/servlet/EndSessionServlet.java
@@ -0,0 +1,19 @@
+package cn.jhc.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name="end session",urlPatterns= {"/endsession.do"})
+public class EndSessionServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		req.getSession().invalidate();
+	}
+}
