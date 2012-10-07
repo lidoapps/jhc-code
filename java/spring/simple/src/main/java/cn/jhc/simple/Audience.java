5a8eeb39377700a4a216838a89d673f7f02e0f10
luyanfei78@gmail.com <luyanfei78@gmail.com@614c841e-ecf4-75f3-70a6-111a6c103a0b>
2012-10-07 12:41:31 +0000

    spring/simple delete target directory.

--- java/spring/simple/src/main/java/cn/jhc/simple/Audience.java
+++ java/spring/simple/src/main/java/cn/jhc/simple/Audience.java
@@ -1,5 +1,7 @@
 package cn.jhc.simple;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+
 public class Audience {
 	
 	//Before performance
@@ -21,5 +23,21 @@ public class Audience {
 	public void demandRefund() {
 		System.out.println("Boo! We want our money back!");
 	}
-
+ 
+	//Around performance
+	public void watchPerformance(ProceedingJoinPoint joinPoint) {
+		try {
+			System.out.println("The audience is taking their seats.");
+			System.out.println("The audience is turning off their cellphone.");
+			long start = System.currentTimeMillis();
+			
+			joinPoint.proceed();
+			
+			long end = System.currentTimeMillis();
+			System.out.println("CLAP CLAP CLAP CLAP CLAP");
+			System.out.println("The performance took " + (end - start) + " milliseconds.");
+		} catch (Throwable e) {
+			System.out.println("Boo! We want our money back.");
+		}
+	}
 }
